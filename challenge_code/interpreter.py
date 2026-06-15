67814e06731c50f0fbd9161b7b92e7701150d51a
cknight51 <139079736+cknight51@users.noreply.github.com>
2026-06-15 11:08:20 -0500

    Refactor main function for improved clarity

--- challenge_code/interpreter.py
+++ challenge_code/interpreter.py
@@ -1,10 +1,10 @@
-def get_expression():
-    # INPUT
+def main():
     while True:
+        # INPUT
         # Prompt the user to enter the expression until valid
         expression = input("Expression: ")
         
-        # PROCESS
+        # Validate the following:
         # Check if the user properly separated the values and operator with spaces
         data = expression.split(" ")
         if len(data) != 3:
@@ -25,40 +25,31 @@ def get_expression():
         if y not in valid_operators:
             print("ERROR: Y must be a valid operator (+, -, *, /)")
             continue
-
-        # Check if the user divided by 0, if so, print divide by 0 error
-        if y == "/" and z == 0:
-            print("ERROR: Cannot divide by 0")
-            continue
-
-        # Enter the valid data into a list [int, str, int]
-        data = [x, y, z]
-        return data
-
-def main():
-    # OUTPUT
-    while True:
-        expression = get_expression()
         
+        # PROCESS
         # Check which operator is being used and convert the expression from a list into a math function
-        if expression[1] == "+":
-            # Preform the math function to find the output
-            result = float(expression[0] + expression[2])
-        elif expression[1] == "-":
-            # Preform the math function to find the output
-            result = float(expression[0] - expression[2])
-        elif expression[1] == "*":
-            # Preform the math function to find the output
-            result = float(expression[0] * expression[2])
+        if y == "+":
+            # Add X to Z to find the result
+            result = float(x + z)
+        elif y == "-":
+            # Subtract Z from X to find the result
+            result = float(x - z)
+        elif y == "*":
+            # Multiply X by Z to find the result
+            result = float(x * z)
         else:
-            # Preform the math function to find the output
-            result = float(expression[0] / expression[2])
+            # Check if the user divided by 0, if so, print divide by 0 error
+            if z == 0:
+                print("ERROR: Cannot divide by 0")
+                continue
+            # Divide X by Z to find to result
+            result = float(x / z)
 
         # Print the output to the user rounded to 1 decimal place
         print(f"{result:.1f}")
 
         # Prompt the user to enter a new expression
-        if input("Would you like to enter a new expression (enter 'y' to continue): ") != "y":
+        if input("Would you like to enter a new expression (enter 'y' to continue): ").lower() != "y":
             break
 
 main()
