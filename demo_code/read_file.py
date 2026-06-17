57f21ac2c670a30d3acecc5ed5ea31745237061b
cknight51 <139079736+cknight51@users.noreply.github.com>
2026-06-17 09:12:48 -0500

    Refactor read_file.py to improve file handling

--- demo_code/read_file.py
+++ demo_code/read_file.py
@@ -1,6 +1,6 @@
 def main():
     # Open menu.txt: Create a file handler to open a file in read mode
-    data_file = open("menu.txt")
+    data_file = open("demo_code\menu.txt")
     print(data_file)
 
     # Create an empty dictionary
@@ -12,11 +12,15 @@ def main():
         item_and_price = line.split(",")
         # Get the item and price from the list
         item = item_and_price[0]
-        price = item_and_price[1]
+        price = float(item_and_price[1])
         # Create an entry in the dictionary for the item and price
         menu[item] = price
 
     # Close the file
-
+    data_file.close()
+    
+    # Print all items from the dictionary
+    for items, prices in menu.items():
+        print(f"{items}: ${prices:.2f}")
 
 main()
