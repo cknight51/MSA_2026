4f3f4ce86d6fa0b059446af4316cf07fd63e6ed7
cknight51 <139079736+cknight51@users.noreply.github.com>
2026-06-22 08:58:52 -0500

    Refactor student data loading and error handling
    Updated the student data processing logic to correctly iterate over lines and append valid students to the list.

--- challenge_code/OOP/student_generator.py
+++ challenge_code/OOP/student_generator.py
@@ -8,20 +8,20 @@ def main():
     students = []
 
     # Iterate of each line of students.csv and split on commas
-    for line in range(1, len(data_file)):
+    for line in data_file:
         student_data = line.split(",")
         if len(student_data) != 6:
-            print(f"ERROR: Invalid formatting on Line {line}")
+            print(f"ERROR: Invalid formatting on Line {students.index(line)}")
             continue
         try:
-            student = Student(student_data[0], student_data[1], student_data[2], int(student_data[3]), student_data[4], student_data[5])
+            student: Student = Student(student_data[0], student_data[1], student_data[2], int(student_data[3]), student_data[4], student_data[5])
         except:
             print(f"ERROR: {student_data[3]} is not a valid number of credit hours")
             continue
-        
 
-
-
-
+        students.append(student)
+    
+    for student in students:
+        student.print_student_data()
 
 main()
