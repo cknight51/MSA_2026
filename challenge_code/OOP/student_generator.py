09463b9a1ae23f61d75f405995df02591c054808
cknight51 <139079736+cknight51@users.noreply.github.com>
2026-06-22 10:46:38 -0500

    Refactor student data import and error handling
    Updated the student data processing logic to improve error handling and formatting checks.

--- challenge_code/OOP/student_generator.py
+++ challenge_code/OOP/student_generator.py
@@ -8,23 +8,32 @@ def main():
     students = []
 
     # Iterate of each line of students.csv and split on commas
-    line_count = -1
+    line_number = 0
     for line in data_file:
-        line_count += 1
-        if line_count == 0:
-            continue
-        student_data = line.split(",")
-        if len(student_data) != 6:
-            print(f"ERROR: Invalid formatting: {line}")
-            continue
-        try:
-            student: Student = Student(student_data[0], student_data[1], student_data[2], int(student_data[3]), student_data[4], student_data[5])
-        except:
-            print(f"ERROR: {student_data[3]} is not a valid number of credit hours")
+        # Skip the header line
+        line_number += 1
+        if line_number == 1:
             continue
 
+        # Split the line of data and make a list of student data
+        student_data = line.split(",")
+
+        # Check if the list has the proper amount of items
+        if len(student_data) != 6:
+            print(f"ERROR: Invalid formatting on Line {line_number} of the file. Data has {len(student_data)} items but should have 6.\n")
+            continue
+            
+        # Try to make a Student, if failed, print error statement 
+        try:
+            student: Student = Student(student_data[0], student_data[1], student_data[2], int(student_data[3]), float(student_data[4]), student_data[5])
+        except:
+            print(f"ERROR: Invalid formatting on Line {line_number}")
+            continue
+
+        # Add student to the list of students
         students.append(student)
-    
+
+    # Print each student's data
     for student in students:
         student.print_student_data()
 
