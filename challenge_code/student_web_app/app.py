dd62acc50e34354f667300ff332a5acd1fd85887
cknight51 <139079736+cknight51@users.noreply.github.com>
2026-06-26 10:18:43 -0500

    Implement major and class search routes
    Added routes for major and class search pages with form handling.

--- challenge_code/student_web_app/app.py
+++ challenge_code/student_web_app/app.py
@@ -33,9 +33,59 @@ def majors_get():
     majors = get_student_data(url)
 
     # Send the list of majors to the majors template to populate the menu
-    return render_template("majors.html", majors=majors)
+    return render_template("major.html", majors=majors)
 
 # Create a route for the major search page to respond to post requests after the form is submitted
+@app.route("/majors", methods=["POST"])
+def majors_post():
+    # Get the list of majors
+    url = "http://127.0.0.1:5000/api/majors/all"
+    majors = get_student_data(url)
+
+    # Get the form data (the chosen major from the select menu)
+    major = request.form.get("major")
+
+    # If major input is invalid display error message and reload page
+    if major == "":
+        flash("ERROR: You must select a major")
+        return redirect(url_for("majors_get"))
+
+    # Get students from that major
+    url = f"http://127.0.0.1:5000/api/majors/{major}"
+    students = get_student_data(url)
+
+    # Send the list of students in the requested major to the majors template to be displayed in the browser
+    return render_template("major.html", majors=majors, students=students, major=major)
+
+# Create a route for the class search page to respond to get requests
+@app.route("/class")
+def class_get():
+    # Make a list of classes
+    classes = ["Freshman", "Sophomore", "Junior", "Senior"]
+
+    # Send the list of classes to the class template to populate the menu
+    return render_template("class.html", classes=classes)
+
+# Create a route for the class search page to respond to post requests after the form is submitted
+@app.route("/class", methods=["POST"])
+def class_post():
+    # Make a list of classes
+    classes = ["Freshman", "Sophomore", "Junior", "Senior"]
+
+    # Get the form data (the chosen major from the select menu)
+    class_level = request.form.get("class")
+
+    # If class input is invalid display error message and reload page
+    if class_level == "":
+        flash("ERROR: You must select a class")
+        return redirect(url_for("class_get"))
+
+    # Get students from that major
+    url = f"http://127.0.0.1:5000/api/class/{class_level}"
+    students = get_student_data(url)
+
+    # Send the list of classes to the class template to populate the menu
+    return render_template("class.html", classes=classes, students=students, class_level=class_level)
 
 
 #run the flask app
